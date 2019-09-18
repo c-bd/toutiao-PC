067efcd275f618849da733b84b06b761278a0db9
c-bd <18334794147@qq.com>
2019-09-18 20:20:08 +0800

    复选框规则，手机号验证码效验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,7 +18,7 @@
           <el-checkbox v-model="loginForm.agree" class="box-card-checked">我已同意以下条款</el-checkbox>
           </el-form-item>
           <el-form-item>
-          <el-button type="primary" style="width:100%">主要按钮</el-button>
+          <el-button type="primary" style="width:100%">提交</el-button>
           </el-form-item>
 
         </el-form>
@@ -30,6 +30,12 @@
 <script>
 export default {
   data () {
+    //   我们在这里定义复选框的规则
+    // validator验证器(自定义效验函数 帮我们效验复选框)
+    // rule当前规则 value当前值 callback回调函数
+    let validator = function (rule, value, callback) {
+      value ? callback() : callback(new Error('请输入正确的信息'))
+    }
     return {
       loginForm: {
         //   1.写出内容
@@ -39,8 +45,11 @@ export default {
       },
       loginRules: {
         // 5.表单验证功能的最终实现 设置为true则表示该字段必填
-        mobile: [{ required: true, message: '请正确输入手机号' }],
-        code: [{ required: true, message: '请正确输入验证码' }]
+        mobile: [{ required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请正确输入手机号' }],
+        code: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '请正确输入验证码,为6位数字' }],
+        agree: [{ validator }]
       }
     }
   }
