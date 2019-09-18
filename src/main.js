c4697256143db57ed391713385633e210dea0381
c-bd <18334794147@qq.com>
2019-09-18 18:09:16 +0800

    引入asios和element-ui模块完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.less'
 Vue.config.productionTip = false
-
+Vue.proyotype.$axios = Axios
+Vue.use(ElementUI)
+Vue.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 new Vue({
   router,
   render: h => h(App)
