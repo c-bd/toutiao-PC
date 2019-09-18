e0fcb88c68b0a7bd07be598ac9982ec73b895556
c-bd <18334794147@qq.com>
2019-09-18 17:44:33 +0800

    默认样式修改完成

--- src/router.js
+++ src/router.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+// 在入口处引入一级路由
+import Home from './views/home/' // 简写
+import Login from './views/login/'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      // 这里两个都是一级路由 我们为空的时候应该默认跳转到首页
       path: '/',
-      name: 'home',
+      directives: 'home'
+
+    },
+    {
+      path: '/home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      component: Login
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
