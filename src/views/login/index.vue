87d9ef8368ebb5db1f80dc188a534833eec483b2
c-bd <18334794147@qq.com>
2019-09-18 21:11:15 +0800

    登陆页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
         <div class="title"><img src="../../assets/img/logo_index.png" alt="">
         </div>
         <!--2.在form上设置:model="loginForm" :rules="loginRules"属性-->
-        <el-form :model="loginForm" :rules="loginRules">
+        <el-form :model="loginForm" :rules="loginRules" ref="ruleForm">
         <!--3.设置prop为里面的内容-->
           <el-form-item style="margin-top:20px"  prop="mobile">
             <!--4.设置v-model为下面data中的内容-->
@@ -15,10 +15,10 @@
                  <el-button style="float:right">请输入验证码</el-button>
           </el-form-item>
           <el-form-item>
-          <el-checkbox v-model="loginForm.agree" class="box-card-checked">我已同意以下条款</el-checkbox>
+                <el-checkbox v-model="loginForm.agree" class="box-card-checked">我已同意以下条款</el-checkbox>
           </el-form-item>
           <el-form-item>
-          <el-button type="primary" style="width:100%">提交</el-button>
+          <el-button type="primary" @click="login()" style="width:100%">提交</el-button>
           </el-form-item>
 
         </el-form>
@@ -33,8 +33,8 @@ export default {
     //   我们在这里定义复选框的规则
     // validator验证器(自定义效验函数 帮我们效验复选框)
     // rule当前规则 value当前值 callback回调函数
-    let validator = function (rule, value, callback) {
-      value ? callback() : callback(new Error('请输入正确的信息'))
+    let validator = function (rule, value, callBack) {
+      value ? callBack() : callBack(new Error('您必须同意无条件被我们蒙骗'))
     }
     return {
       loginForm: {
@@ -51,6 +51,35 @@ export default {
           { pattern: /^\d{6}$/, message: '请正确输入验证码,为6位数字' }],
         agree: [{ validator }]
       }
+    }
+  },
+  methods: {
+    login () {
+    // 效验全部表单的方法 通过获取整个表单的dom节点 然后通过一个回调函数来验证 函数的参数有两个 是否成功和未通过效验的字段
+      this.$refs.ruleForm.validate((isOk) => {
+        if (isOk) {
+          // 如果成功则发送sxios请求 将用户信息携带到主页中去
+          this.$axios({
+            url: '/authorizations',
+            method: 'post',
+            // 提交的内容是表单的数据
+            data: this.loginForm
+          }).then(
+            result => {
+            //   我们这里需要把这个值存入本地存储器中 token令牌 存储用户的信息
+              window.localStorage.setItem('user-token', result.data.data.token)
+              this.$router.push('./home')
+            }
+          ).catch(
+            () => {
+              this.$message({
+                message: '手机号或者验证码错误',
+                type: 'warning'
+              })
+            }
+          )
+        }
+      })
     }
   }
 }
