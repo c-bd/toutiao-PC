32218b803a58455ca88ebea168ce03b28376314c
c-bd <18334794147@qq.com>
2019-09-18 20:02:49 +0800

    手机号和验证码未输入提示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,12 +3,16 @@
         <el-card class="box-card">
         <div class="title"><img src="../../assets/img/logo_index.png" alt="">
         </div>
-        <el-form>
-          <el-form-item style="margin-top:20px">
-                <el-input></el-input>
+        <!--2.在form上设置:model="loginForm" :rules="loginRules"属性-->
+        <el-form :model="loginForm" :rules="loginRules">
+        <!--3.设置prop为里面的内容-->
+          <el-form-item style="margin-top:20px"  prop="mobile">
+            <!--4.设置v-model为下面data中的内容-->
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号" ></el-input>
           </el-form-item>
-          <el-form-item style="width:65%">
-                <el-input></el-input>
+          <el-form-item  prop="code">
+                <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:60%"></el-input>
+                 <el-button style="float:right">请输入验证码</el-button>
           </el-form-item>
           <el-form-item>
           <el-checkbox v-model="loginForm.agree" class="box-card-checked">我已同意以下条款</el-checkbox>
@@ -28,9 +32,15 @@ export default {
   data () {
     return {
       loginForm: {
+        //   1.写出内容
         mobile: '', // 手机号
         code: '', // 验证码
         agree: false // 是否同意协议
+      },
+      loginRules: {
+        // 5.表单验证功能的最终实现 设置为true则表示该字段必填
+        mobile: [{ required: true, message: '请正确输入手机号' }],
+        code: [{ required: true, message: '请正确输入验证码' }]
       }
     }
   }
