9223e2d5285f261d2e5432331702c45d1999e2a7
c-bd <18334794147@qq.com>
2019-09-18 21:37:25 +0800

    完成登录界面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>首页</div>
+
+    <el-container >
+        <el-aside >
+        <!--左侧导航组件-->
+             <layout-aside></layout-aside>
+        </el-aside>
+        <!--右侧导航组件-->
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+
 </template>
 
 <script>
+import layoutAside from '../../components/home/layou-aside'
 export default {
-
+  comments: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+
 </style>
