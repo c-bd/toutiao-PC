5c5f0d6657d3285b222a1a5d1e37e6c76e2eae4a
c-bd <18334794147@qq.com>
2019-09-21 19:59:19 +0800

    评论列表框架完成

--- src/components/content/home-children/home-comment.vue
+++ src/components/content/home-children/home-comment.vue
@@ -1,11 +1,14 @@
 <template>
+<el-card shadow="hover">
     <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item>
-        <!--这里是上面后面的信息 我们在这里做一个插槽 因为并不是固定的 所以我们定义一个具名插槽-->
+        <!--这里是上面后面的信息 我们在这里做一个插槽 因为并不是固定的 所以我们定义一个具名插槽  因为每一页都是独立的 需要我们单独去开发  所以就在这里定义一个插槽容器 所有的都可以找到这里-->
             <slot name="title"></slot>
         </el-breadcrumb-item>
     </el-breadcrumb>
+</el-card>
+
 </template>
 
 <script>
@@ -14,6 +17,6 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style>
 
 </style>
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,13 +1,51 @@
 <template>
+<el-card>
+    <layout-right-content>
+        <template slot="title">评论列表</template>
+    </layout-right-content>
+    <!--下面用一个 table表格来收集和展示内容-->
+      <el-table
+    :data="list">
+    <el-table-column
+      prop="date"
+      label="标题"
+      width="580">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="评论状态"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="总评论数"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="fans-comment"
+      label="粉丝评论数"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="operate"
+      label="操作"
+      >
+    </el-table-column>
+  </el-table>
+</el-card>
 
-<layout-right-content>
-    <template slot="title">评论列表</template>
-</layout-right-content>
 </template>
-
 <script>
 export default {
+  data () {
+    return {
 
+    }
+  },
+  methods: {
+    //   我们在这里需要拉取数据将评论展示在评论列表内
+
+  }
 }
 </script>
 
