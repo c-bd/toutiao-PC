a031494d78d33975b0a773327c0336ddf54c1e5e
c-bd <18334794147@qq.com>
2019-09-23 21:57:27 +0800

    内容列表搜索和删除完成（页面完成）

--- src/views/contents/index.vue
+++ src/views/contents/index.vue
@@ -3,9 +3,9 @@
         <layout-right-content>
             <template slot="title">内容列表</template>
         </layout-right-content>
-        <el-form class="artical" style="margin-left:50px">
+        <el-form class="artical" style="margin:50px">
             <el-form-item label="文章列表:">
-                <el-radio-group v-model="formData.status" style="margin-left:20px" >
+                <el-radio-group v-model="formData.status" style="margin-left:20px" @change="changeCondition">
                      <el-radio-button  label="5">全部</el-radio-button>
                      <el-radio-button  label="0">草稿</el-radio-button>
                      <el-radio-button  label="1">待审核</el-radio-button>
@@ -39,7 +39,6 @@
               end-placeholder="结束日期">
             </el-date-picker>
           </el-form-item>
-          {{formData.status}}{{formData.channel_id}}{{formData.data}}
         </el-form>
         <el-card class="box-card" :body-style="{ padding: '0px' , borderBottom:'5px solid #f2f3f4'}">
           <div slot="header" class="clearfix">
@@ -57,7 +56,7 @@
                 </div>
                 <div class="img-item-right">
                     <el-button type="text" style="color:black"><i class="el-icon-edit"></i>修改</el-button>
-                    <el-button type="text" style="color:black"><i class="el-icon-delete" @click="delectMaterial(item.id)"></i>删除</el-button>
+                    <el-button type="text" style="color:black"  @click="delectMaterial(item.id)"><i class="el-icon-delete"></i>删除</el-button>
                 </div>
           </div>
     </el-card>
@@ -127,6 +126,37 @@ export default {
     }
   },
   methods: {
+    //   删除素材
+    delectMaterial (id) {
+      this.$confirm('您确定要删除吗？').then(() => {
+        this.$axios({
+          url: `/articles/${id.toString()}`,
+          method: 'delete'
+        }).then(result => {
+          this.queryArticles()
+        })
+      })
+    },
+    // 这里是当上面搜索内容改变的时候，发生的事件 我们需要发送请求参数然后向后台发送请求  将满足条件的结果返回
+    changeCondition () {
+      this.page.currentPage = 1
+      this.queryArticles()
+      // 这里我们搜索的时候不论在第几页当点击改变时 都应该出现在对应的位置的当前页为第一页才合理
+      // let pages = this.page.currentPage = 1
+      // 我们通过接口找到了需要请求的参数  我们把这些参数定义一个集合  然后请求参数返回结果
+    },
+    // 改变的时候都需要掉这个接口 所以我们把他抽离出来  就是切换页码的时候我们需要把当前页最新的页码传过去而切换的时候我们应该把第一页传过去
+    queryArticles () {
+      let params = {
+        channel_id: this.formData.channel_id,
+        status: this.formData.status === 5 ? null : this.formData.status, // 因为是数字 所以应该传一个空null这样才可以返回全部数据 因为默认传空返回全部
+        begin_pubdate: this.formData.data.length > 0 ? this.formData.data[0] : null, // 因为得到的时间是一个数组 我们需要判断起止时间来做出判断
+        end_pubdate: this.formData.data.length > 1 ? this.formData.data[1] : null,
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
+      this.getCountent(params)
+    },
     // 获取频道列表
     getChannels () {
       this.$axios({
@@ -135,32 +165,23 @@ export default {
         this.options = result.data.channels
       })
     },
-    //   删除素材
-    delectMaterial (id) {
-      this.$confirm('您确定要删除吗？').then(() => {
-        this.$axios({
-          url: `/articles/${id.toString()}`,
-          method: 'delete'
-        }).then(result => {
 
-        })
-      })
-    },
     handleCurrentChange (val) {
       this.page.currentPage = val
-      this.getCountent()
-      //  这时候当我们改变页码时 会传入一个参数这个参数就是当前页码的数  我们把这个数重新传入  然后发送请求  就会把这一夜的数据给你返回来
+      this.queryArticles()
+      //  这时候当我们改变页码时 会传入一个参数这个参数就是当前页码的数  我们把这个数重新传入  然后发送请求  就会把这一页的数据给你返回来
     },
     // 改变一页几条
     handleSizeChange (val) {
       this.page.pageSize = val
-      this.getCountent()
+      this.queryArticles()
     },
-    getCountent () {
+    // 获取全部文章内容数据
+    getCountent (params) {
       this.loading = true
       this.$axios({
         url: '/articles',
-        params: { page: this.page.currentPage, per_page: this.page.pageSize }
+        params
       }).then(result => {
         this.list = result.data.results
         this.page.total = result.data.total_count
