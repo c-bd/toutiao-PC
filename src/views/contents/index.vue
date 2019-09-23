b429b68f35916dcebc3ef90b5a96f41dcea2085f
c-bd <18334794147@qq.com>
2019-09-23 19:57:59 +0800

    内容列表上部样式完成

--- src/views/contents/index.vue
+++ src/views/contents/index.vue
@@ -1,39 +1,65 @@
 <template>
     <el-card slot="title" v-loading="loading">
-<layout-right-content>
-    <template slot="title">内容列表</template>
-</layout-right-content>
-<div class="artical">
-    <span>文章状态:</span>
-    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-    <span>频道列表</span>
-    <el-tab-pane label="全部" name="first" class="el-pane"></el-tab-pane>
-        <el-tab-pane label="草稿" name="second" class="el-pane"></el-tab-pane>
-        <el-tab-pane label="待审核" name="third" class="el-pane"></el-tab-pane>
-        <el-tab-pane label="审核通过" name="fourth" class="el-pane"></el-tab-pane>
-        <el-tab-pane label="审核失败" name="five" class="el-pane"></el-tab-pane>
-  </el-tabs>
-</div>
-<el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>总共找到{{page.total}}条符合条件的内容</span>
-  </div>
-  <el-card class="img-item" v-for="item in list" :key="item.id" :body-style="{ padding: '0px'}">
-        <div class="img-item-left">
-            <img src="../../assets/img/avatar.jpg">
-            <div class="img-item-x">
-                <p style="margin:0">{{item.title}}</p>
-                <el-button type="primary" plain size="small" style="margin:10px 0">{{item.status==="1"? "待审核":"未通过"}}</el-button>
-                <p style="margin:5px 0">{{item.pubdate}}</p>
-            </div>
-        </div>
-        <div class="img-item-right">
-            <el-button type="text" style="color:black"><i class="el-icon-edit"></i>修改</el-button>
-            <el-button type="text" style="color:black"><i class="el-icon-delete"></i>删除</el-button>
-        </div>
-  </el-card>
-</el-card>
-<div class="block">
+        <layout-right-content>
+            <template slot="title">内容列表</template>
+        </layout-right-content>
+        <el-form class="artical" style="margin-left:50px">
+            <el-form-item label="文章列表:">
+                <el-radio-group v-model="formData.status" style="margin-left:20px" >
+                     <el-radio-button  label="5">全部</el-radio-button>
+                     <el-radio-button  label="0">草稿</el-radio-button>
+                     <el-radio-button  label="1">待审核</el-radio-button>
+                     <el-radio-button  label="2">审核通过</el-radio-button>
+                     <el-radio-button  label="3">审核失败</el-radio-button>
+                </el-radio-group>
+            </el-form-item>
+        </el-form>
+        <el-form style="margin-left:50px">
+             <el-form-item label="频道列表:">
+                <el-select v-model="formData.channel_id" clearable placeholder="请选择" style="margin-left:20px">
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+             </el-form-item>
+        </el-form>
+        <el-form style="margin-left:50px">
+          <el-form-item label="时间选择:" >
+          <el-date-picker
+              style="margin-left:20px"
+              v-model="formData.data"
+              type="daterange"
+              prefix-icon="el-icon-time"
+              value-format="yyyy-MM-dd"
+              range-separator="-"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期">
+            </el-date-picker>
+          </el-form-item>
+        </el-form>
+        <el-card class="box-card" :body-style="{ padding: '0px' , borderBottom:'5px solid #f2f3f4'}">
+          <div slot="header" class="clearfix">
+            <span>总共找到{{page.total}}条符合条件的内容</span>
+          </div>
+          <div class="img-item" v-for="item in list" :key="item.id">
+                <div class="img-item-left">
+                    <img :src="item.cover.images.lenght ? item.cover.images[0] : defaultimages">
+                    <div class="img-item-x">
+                        <p style="margin:0 ; font-size:16px">{{item.title}}</p>
+                        <el-tag type="primary" plain size="small" style="margin:10px 0">{{item.status==="1"? "待审核":"未通过"}}</el-tag>
+                        <p style="margin:5px 0;font-size:12px">{{item.pubdate}}</p>
+                    </div>
+                </div>
+                <div class="img-item-right">
+                    <el-button type="text" style="color:black"><i class="el-icon-edit"></i>修改</el-button>
+                    <el-button type="text" style="color:black"><i class="el-icon-delete" @click="delectMaterial"></i>删除</el-button>
+                </div>
+          </div>
+    </el-card>
+
     <el-row type="flex" justify="center">
     <el-pagination
       background
@@ -45,7 +71,7 @@
       :total="page.total">
     </el-pagination>
     </el-row>
-  </div>
+
 </el-card>
 </template>
 
@@ -53,17 +79,26 @@
 export default {
   data () {
     return {
-      activeName: 'first',
+      defaultimages: require('../../assets/img/avatar.jpg'),
       list: [],
       loading: false,
       page: {
         total: 0, // 总页数
         currentPage: 1, // 当前页
         pageSize: 10// 一页几条
+      },
+      formData: {// 我们这里定义一个
+        status: 5, // 文章状态默认值我们让她为全部
+        channel_id: null, // 频道id值  默认为null空 因为是数字类型  所以就不能写成空字符串
+        data: []// 时间  后台返回的是一个数组  所以这样定义
       }
     }
   },
   methods: {
+    //   删除素材
+    delectMaterial () {
+
+    },
     handleCurrentChange (val) {
       this.page.currentPage = val
       this.getCountent()
@@ -74,14 +109,11 @@ export default {
       this.page.pageSize = val
       this.getCountent()
     },
-    handleClick (tab, event) {
-      console.log(tab, event)
-    },
     getCountent () {
       this.loading = true
       this.$axios({
         url: '/articles',
-        params: { response_type: 'statistic', page: this.page.currentPage, per_page: this.page.pageSize }
+        params: { page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
         this.page.total = result.data.total_count
@@ -97,17 +129,15 @@ export default {
 
 <style lang="less" scoped>
 .artical{
-    display:flex;
-    align-items:center;
-    span{
-        margin-right:30px;
-    }
 }
 .box-card{
     margin-top:100px;
 }
 .img-item{
-
+    padding:20px 10px;
+    border-bottom:1px solid #f2f3f5;
+     display:flex;
+     justify-content:space-between;
     .img-item-left{
         display:flex;
         .img-item-x{
@@ -116,11 +146,11 @@ export default {
         img{
             width:150px;
             height:100px;
+            border-radius:4px;
         }
     }
     .img-item-right{
-        float:right;
-
+        height:100px;
     }
 }
 </style>
