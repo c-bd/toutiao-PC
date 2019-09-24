4fbad67b04cf40ac9ee28880360295f45c645d44
c-bd <18334794147@qq.com>
2019-09-24 19:46:04 +0800

    发表文章界面封面创建完成

---
+++ src/components/pubulish/cover-images.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class='cover-image'>
+        <div class='cover-item' v-for="(item,index) in image" :key="index">
+            <img src="../../assets/img/pic_bg.png" alt="">
+        </div>
+        <!-- 现在我们需要循环 当循环的时候将下面的图片渲染出来 点击前面的封面选项 判断当前图片数组内有无图片 如果没有则生成0个标签  其他情况则生成多个标签 -->
+
+    </div>
+</template>
+
+<script>
+export default {
+  // 这里我们需要把状态吗对应的照片的数组传过来
+  props: ['image'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.cover-image{
+   display: flex;
+   margin: 22px;
+   margin-left:100px;
+    .cover-item{
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+}
+</style>
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,23 +1,30 @@
 <template>
-<el-card slot="title">
+<el-card slot="header">
     <layout-right-content>
             <template slot="title">发布文章</template>
     </layout-right-content>
-    <el-form style='margin-left:100px' label-width="100px">
-        <el-form-item label="标题" style='width:400px'>
-            <el-input></el-input>
+    <el-form style='margin-left:35px' label-width="100px" :model="loginForm">
+        <el-form-item label="标题" style='width:400px' prop="title">
+            <el-input v-model="loginForm.title"></el-input>
         </el-form-item>
-        <el-form-item label="内容">
-           <el-radio-group>
-                    <el-radio>单选</el-radio>
-                    <el-radio>三图</el-radio>
-                    <el-radio>无图</el-radio>
-                    <el-radio>自动</el-radio>
-                </el-radio-group>
+        <el-form-item label="内容" prop="content">
+        <!--自定义富文本编辑器组件-->
+           <quitor-editor v-model="loginForm.content" style="height:300px"></quitor-editor>
         </el-form-item>
-        <el-form-item label="频道">
-           <el-select>
-                <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
+        <el-form-item label="封面" prop="cover" style='margin-top:120px'>
+          <!-- 我们给这个注册一个值改变事件 这样就可以监听值改变时的状态 -->
+           <el-radio-group  v-model="loginForm.cover.type" @change="getCover">
+              <el-radio :label="1">单选</el-radio>
+              <el-radio :label="3">三图</el-radio>
+              <el-radio :label="0">无图</el-radio>
+              <el-radio :label="-1">自动</el-radio>
+           </el-radio-group>
+        </el-form-item>
+        <!-- 父给子传递数据时 我们在父组件中写上要传递的值 在子中通过prop属性来获取 然后渲染到页面上 -->
+        <cover-image :image="loginForm.cover.images"></cover-image>
+        <el-form-item label="频道" prop="channel">
+           <el-select v-model="loginForm.shannel">
+                <el-option v-for="(item,index) in channels" :key="index" :value="item.id" :label="item.name"></el-option>
             </el-select>
         </el-form-item>
         <el-form-item >
@@ -33,10 +40,29 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      loginForm: {
+        title: '',
+        cover: {
+          type: 0,
+          images: []
+        }
+
+      }
     }
   },
   methods: {
+    // 封面内容改变时获取label的内容
+    getCover () {
+      // 值改变的时候 我们判断当前的type的状态是什么  如果是0.-1那么images数组的长度为0 1的时候数组长度为1  3的时候数组长度为3
+      if (this.loginForm.cover.type === 1) {
+        this.loginForm.cover.images = ['']
+      } else if (this.loginForm.cover.type === 3) {
+        this.loginForm.cover.images = ['', '', '']
+      } else {
+        this.loginForm.cover.images = []
+      }
+    },
     //   获取频道的下拉内容
     getChannels () {
       this.$axios({
