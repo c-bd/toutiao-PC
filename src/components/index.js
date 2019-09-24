cb07e5dd16faef828169e91058893eb628185a92
c-bd <18334794147@qq.com>
2019-09-24 22:25:10 +0800

    新建封面弹窗模块创建完成

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import layoutAside from './home/layou-aside'
 import layoutHeader from './header/layou-header'
 import layoutContent from './content/home-comment.vue'
 import layoutpubishImg from './pubulish/cover-images.vue'// 文章列标上传图片
+import layoutPopImg from './pubulish/pup-images.vue'
 // 全局注册富文本编辑器
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
@@ -10,10 +11,11 @@ import 'quill/dist/quill.bubble.css'
 import { quillEditor } from 'vue-quill-editor'
 export default {
   install (Vue) {
-    Vue.component('layout-aside', layoutAside)
-    Vue.component('layout-right-header', layoutHeader)
-    Vue.component('layout-right-content', layoutContent)
-    Vue.component('quitor-editor', quillEditor)
-    Vue.component('cover-image', layoutpubishImg)
+    Vue.component('layout-aside', layoutAside)// 全局注册左侧导航组件
+    Vue.component('layout-right-header', layoutHeader)// 全局注册头部组件
+    Vue.component('layout-right-content', layoutContent)// 全局注册面包屑组件
+    Vue.component('quitor-editor', quillEditor)// 全局注册富文本编辑器
+    Vue.component('cover-image', layoutpubishImg)// 全局注册封面组件
+    Vue.component('pop-image', layoutPopImg)// 全局注册封面组件
   }
 }
--- src/components/pubulish/cover-images.vue
+++ src/components/pubulish/cover-images.vue
@@ -1,10 +1,13 @@
 <template>
     <div class='cover-image'>
-        <div class='cover-item' v-for="(item,index) in image" :key="index">
+        <div class='cover-item' v-for="(item,index) in image" :key="index" @click="uploadImg">
             <img src="../../assets/img/pic_bg.png" alt="">
         </div>
         <!-- 现在我们需要循环 当循环的时候将下面的图片渲染出来 点击前面的封面选项 判断当前图片数组内有无图片 如果没有则生成0个标签  其他情况则生成多个标签 -->
-
+        <el-dialog :visible="dialogVisible">
+                <!-- 这里放弹窗出来的内容 -->
+                <pop-image></pop-image>
+        </el-dialog>
     </div>
 </template>
 
@@ -14,7 +17,12 @@ export default {
   props: ['image'],
   data () {
     return {
-
+      dialogVisible: false
+    }
+  },
+  methods: {
+    uploadImg () {
+      this.dialogVisible = true
     }
   }
 }
@@ -28,6 +36,11 @@ export default {
     .cover-item{
         border: 1px solid #ccc;
         padding: 10px;
+        img{
+            width: 100%;
+            height: 100%;
+            border:1px solid #f2f5c2;
+        }
     }
 }
 </style>
