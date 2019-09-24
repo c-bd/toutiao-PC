8082e3c225ca3c43678c1e4bb847e514e8d7692b
c-bd <18334794147@qq.com>
2019-09-24 20:27:31 +0800

    发表文章表单验证完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,7 +3,8 @@
     <layout-right-content>
             <template slot="title">发布文章</template>
     </layout-right-content>
-    <el-form style='margin-left:35px' label-width="100px" :model="loginForm">
+    <!-- 验证表单 -->
+    <el-form style='margin-left:35px' label-width="100px" :model="loginForm" :rules="rules" ref="ruleForm">
         <el-form-item label="标题" style='width:400px' prop="title">
             <el-input v-model="loginForm.title"></el-input>
         </el-form-item>
@@ -22,14 +23,15 @@
         </el-form-item>
         <!-- 父给子传递数据时 我们在父组件中写上要传递的值 在子中通过prop属性来获取 然后渲染到页面上 -->
         <cover-image :image="loginForm.cover.images"></cover-image>
-        <el-form-item label="频道" prop="channel">
-           <el-select v-model="loginForm.shannel">
+        <el-form-item label="频道" prop="channel_id">
+           <el-select v-model="loginForm.channel_id">
                 <el-option v-for="(item,index) in channels" :key="index" :value="item.id" :label="item.name"></el-option>
             </el-select>
         </el-form-item>
         <el-form-item >
-             <el-button type='primary'>发布文章</el-button>
-             <el-button>存入草稿</el-button>
+          <!-- 在这里存入草稿和发布文章是一样的 唯一的区别就是点击存入草稿是将draft设置成true -->
+             <el-button type='primary' @click="pubilshArticil(false)">发布文章</el-button>
+             <el-button @click="pubilshArticil(true)">存入草稿</el-button>
         </el-form-item>
     </el-form>
 </el-card>
@@ -42,16 +44,33 @@ export default {
     return {
       channels: [],
       loginForm: {
-        title: '',
+        content: '', // 内容
+        title: '', // 标题
+        channel_id: null, // 频道id
         cover: {
           type: 0,
           images: []
         }
-
+      },
+      rules: {
+        channel_id: [{ required: true, message: '请选择频道' }
+        ],
+        content: [{ required: true, message: '请输入内容' }],
+        title: [{ required: true, message: '请输入标题' },
+        // 这里限制一下标题的最小长度和最大长度
+          { min: 5, max: 30, message: '标题长度要控制在5到30个字符之间' }]
       }
     }
   },
   methods: {
+    // 发表文章和存入草稿  首先我们先验证表单
+    pubilshArticil (draft) {
+      this.$refs.ruleForm.validate((isOK) => {
+        if (isOK) {
+
+        }
+      })
+    },
     // 封面内容改变时获取label的内容
     getCover () {
       // 值改变的时候 我们判断当前的type的状态是什么  如果是0.-1那么images数组的长度为0 1的时候数组长度为1  3的时候数组长度为3
